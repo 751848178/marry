89e6adc5051250ddebdec8f4375606c303552189
751848178@qq.com <751848178@qq.com>
2018-08-03 12:45:14 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # marry
+
+
+## 安装依赖
+
+```shell
+yarn
+
+# or
+
+npm install
+```
+
+# 启动
+
+npm run start
+
+# 数据库
+
+/sql/marry.sql
+
+# 端口
+
+	默认端口：3000
+	
+	修改端口：/bin/www
+	
+		var port = normalizePort(process.env.PORT || '3000'); // 替换3000为目标端口
---
+++ sql/marry.sql
@@ -0,0 +1,36 @@
+/*
+SQLyog Ultimate v8.32 
+MySQL - 5.5.27 : Database - marry
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`marry` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+
+USE `marry`;
+
+/*Table structure for table `guest` */
+
+DROP TABLE IF EXISTS `guest`;
+
+CREATE TABLE `guest` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '登记id',
+  `name` varchar(50) COLLATE utf8_bin NOT NULL COMMENT '登记姓名',
+  `number` int(10) NOT NULL COMMENT '登记人数',
+  `arrivaltime` datetime NOT NULL COMMENT '预计到达时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_bin CHECKSUM=1;
+
+/*Data for the table `guest` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
